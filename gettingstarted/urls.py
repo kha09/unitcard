274a6660aea16be079d7f67a1bc352a930d73a0c
unknown <KHA@DESKTOP-413PLKT.(none)>
2022-07-26 15:58:33 +0300

    start of unit card project

---
+++ gettingstarted/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from django.contrib import admin
+from unitcard.views import *
+
+admin.autodiscover()
+
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home),
+]
