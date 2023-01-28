2feb81d233fa6c961fb5df91a11f2f6582e8366b
unknown <KHA@DESKTOP-413PLKT.(none)>
2023-01-28 23:06:30 +0300

    added database postgres

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VP5q8fBCpmEueW8PYkLz',
+        'HOST': 'containers-us-west-40.railway.app',
+        'PORT': '6605',
     }
 }
 
