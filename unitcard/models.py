0dd2ef48ceb01ef595196cea76d27c94a38cc606
unknown <KHA@DESKTOP-413PLKT.(none)>
2023-02-25 20:02:59 +0300

    added foreign key to Vcard

--- unitcard/models.py
+++ unitcard/models.py
@@ -3,13 +3,7 @@ from django.core.validators import RegexValidator
 import os
 
 # Create your models here.
-class Vcard(models.Model):
-    filecard = models.FileField(upload_to = 'card/')
-    imgcard = models.ImageField(upload_to = 'img/', default='img01.png')
-    # def filename(self):
-    #     return os.path.basename(self.filecard.name)
-    def __str__(self):
-        return self.filecard.name
+
 
 class Card(models.Model):
     first_name = models.CharField(max_length=50)
@@ -22,11 +16,18 @@ class Card(models.Model):
     phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
     email = models.EmailField(max_length=255, blank=True)
     website = models.CharField(max_length=255, blank=True)
-    mcard = models.ForeignKey(Vcard, on_delete=models.CASCADE)
     def __str__(self):
         return self.first_name
 
+class Vcard(models.Model):
+    filecard = models.FileField(upload_to = 'card/')
+    imgcard = models.ImageField(upload_to = 'img/', default='img01.png')
+    mcard = models.ForeignKey(Card, on_delete=models.CASCADE)
 
+    # def filename(self):
+    #     return os.path.basename(self.filecard.name)
+    def __str__(self):
+        return self.filecard.name
 
 
 # class Scard(models.Model):
